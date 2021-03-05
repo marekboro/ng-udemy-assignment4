0ec11706c607d619ce154f12418de0dd6e9635f0
marekboro <marek.borowicz83@gmail.com>
2021-03-05 15:05:13 +0000

    added the components and a counter to run reset on start stop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements ViewChild {
+ descendants: boolean;
+ emitDistinctChangesOnly: boolean;
+ first: boolean;
+ read: any;
+ isViewQuery: boolean;
+ selector: any;
+ static?: boolean;
+
+  counter:number
+
+@Input() incoming:number;  
+
+constructor(){}
+
+
+ngOnChanges(changes: SimpleChanges) {
+  this.counter= this.incoming;
+  console.log(this.counter)
+}
+
 }
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  ref: any;
+
+  counter:number = 0; 
+  @Output() gameCreated = new EventEmitter<{ourCounterState:number}>()
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onStartGame(){
+    this.ref = setInterval(() => {
+      this.counter+=1;
+      console.log(this.counter)
+    },1000);
+  }
+  onEndGame(){
+    clearInterval(this.ref);
+    this.counter=0;
+
+  }
+
+}
